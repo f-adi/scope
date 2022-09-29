f4b2978c7a7c15416be08bf61f306edc950780c6
f-adi <fadi.baaklini@gmail.com>
2022-09-29 06:58:24 -0400

    this

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -34,3 +34,46 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
+
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//Functions
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+//Example
+
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
+// }
+
+var x = 1;
+let y = 2;
+const z = 3;
+*/
+
+console.log(this);
